9073f9e66db0c42c04a72d5d711a3b20fb9a1f73
aariyasreeskumar123 <ariyasree.it.sxcce@gmail.com>
2025-12-12 20:17:39 +0530

    Basics

--- Aggregate_Functions.sql
+++ Aggregate_Functions.sql
@@ -23,6 +23,7 @@ INSERT INTO pupil VALUES("Dev",80,"IT")
 INSERT INTO pupil VALUES("Shruthi",40,"AI/DS")
 INSERT INTO pupil VALUES("Charu",60,"IT")
 INSERT INTO pupil VALUES("Anusha",80,"MECH")
+INSERT INTO pupil VALUES("SARA",99,"CSE")
 
 UPDATE pupil SET Mark=80 WHERE Department="MECH"
 
@@ -32,3 +33,12 @@ UPDATE pupil SET Mark=80 WHERE Department="MECH"
 
 SELECT * FROM pupil 
 
+ --MAX()
+SELECT MAX(Mark) FROM pupil
+SELECT MAX(Mark) AS Maximum_Mark FROM pupil
+SELECT MAX(MARK) FROM pupil WHERE Department="IT"
+SELECT MAX(MARK) AS Maximum_Mark_IT FROM pupil WHERE Department="IT"
+
+--MIN()
+
+SELECT MIN(Mark) FROM pupil 
